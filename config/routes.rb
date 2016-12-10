d1564f2dd6af98df22e2fdda6d5679f1df640737
Yoshihiro Nakazawa <y_nkzw@nifty.ne.jp>
2016-12-10 21:47:17 +0000

    DIVE16-1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'relationships/create'
+
+  get 'relationships/destroy'
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   devise_for :users, controllers: {
@@ -20,6 +24,10 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
+
+  resources :users, only: [:index]
+
+  resources :relationships, only: [:create, :destroy]
 
   root 'top#index'
 
