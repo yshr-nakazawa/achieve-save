b54fc053affa2b70b33cf426023954620d8d9ec8
Yoshihiro Nakazawa <y_nkzw@nifty.ne.jp>
2016-12-11 03:49:22 +0000

    DIVE16-2

--- config/routes.rb
+++ config/routes.rb
@@ -25,7 +25,7 @@ Rails.application.routes.draw do
     end
   end
 
-  resources :users, only: [:index]
+  resources :users, only: [:index, :show]
 
   resources :relationships, only: [:create, :destroy]
 
