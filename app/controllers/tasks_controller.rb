bcacc7fd880a165d92d84de9f20276d90a373d66
Yoshihiro Nakazawa <y_nkzw@nifty.ne.jp>
2016-12-19 03:00:22 +0000

    タスク編集画面・詳細画面をBootstrap化

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,10 +27,10 @@ class TasksController < ApplicationController
   def create
     @task = current_user.tasks.build(task_params)
     @task.charge_id = current_user.id
-    
+
     respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        format.html { redirect_to tasks_path, notice: 'タスクを新規作成しました。' }
         format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new }
@@ -44,7 +44,7 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to @task, notice: 'Task was successfully updated.' }
+        format.html { redirect_to tasks_path, notice: 'タスクを更新しました。' }
         format.json { render :show, status: :ok, location: @task }
       else
         format.html { render :edit }
@@ -58,7 +58,7 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     respond_to do |format|
-      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
+      format.html { redirect_to tasks_url, notice: 'タスクを削除しました。' }
       format.json { head :no_content }
     end
   end
