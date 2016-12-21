87a21a811cda8ffc8d2e47b945f857c2da4f1f42
Yoshihiro Nakazawa <y_nkzw@nifty.ne.jp>
2016-12-21 07:59:00 +0000

    タスク依頼関連画面をBootstrap化

--- app/models/submit_request.rb
+++ app/models/submit_request.rb
@@ -6,6 +6,8 @@ class SubmitRequest < ActiveRecord::Base
   validates :user_id, :task_id, :request_user_id, presence: true
   validate :no_repeated_request, on: :create
 
+  enum status: {依頼中:1, 承認済:2, 却下:9}
+
   private
 
     def no_repeated_request
